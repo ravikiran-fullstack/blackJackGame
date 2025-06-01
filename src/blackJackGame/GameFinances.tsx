fa132ba992ef98a6e3dfbc38da282259ff1a58c5
ravikiran <ravikiranjobs21@gmail.com>
2025-05-31 22:29:43 -0400

    Improved styles in game status and game container

--- src/blackJackGame/GameFinances.tsx
+++ src/blackJackGame/GameFinances.tsx
@@ -4,7 +4,11 @@ import { GameContext } from "../context/blackJackGameContext/GameContext";
 const GameFinances = () => {
   const gameContext = useContext(GameContext);
   console.log('gameContext',gameContext);
-  return <div className="gameFinanceContainer">GameFinances</div>;
+  return <div className="gameFinanceContainer">
+    <div className="header">
+      <h3>Game Finances</h3>
+    </div>
+  </div>;
 };
 
 export default GameFinances;
